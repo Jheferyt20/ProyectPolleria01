1a40b1e39a108c1ac35c42e9c4c9687aa5502271
Hans <edi_fv@hotmail.com>
2024-03-01 18:12:46 -0500

    .add

--- app/src/main/java/com/app/proyectpolleria/ui/dashboard/FavoritoFragment.kt
+++ app/src/main/java/com/app/proyectpolleria/ui/dashboard/FavoritoFragment.kt
@@ -1,4 +1,4 @@
-package com.app.proyectpolleria.ui.home
+package com.app.proyectpolleria.ui.dashboard
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.app.proyectpolleria.databinding.FragmentHomeBinding
+import com.app.proyectpolleria.databinding.FragmentFavoritoBinding
 
-class HomeFragment : Fragment() {
+class FavoritoFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentFavoritoBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+        val favoritoViewModel =
+            ViewModelProvider(this).get(FavoritoViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentFavoritoBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textDashboard
+        favoritoViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
--- app/src/main/java/com/app/proyectpolleria/ui/dashboard/FavoritoViewModel.kt
+++ app/src/main/java/com/app/proyectpolleria/ui/dashboard/FavoritoViewModel.kt
@@ -1,13 +1,13 @@
-package com.app.proyectpolleria.ui.notifications
+package com.app.proyectpolleria.ui.dashboard
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class FavoritoViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is dashboard Fragment"
     }
     val text: LiveData<String> = _text
 }
