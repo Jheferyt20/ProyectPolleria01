c425d4b19181ca0182f08d87b9422f0bba35a747
Gabriel DarKreitos <inazuma123987@gmail.com>
2024-03-15 19:58:40 -0500

    .add

--- app/src/main/java/com/app/proyectpolleria/Activity_login.kt
+++ app/src/main/java/com/app/proyectpolleria/Activity_login.kt
@@ -1,11 +1,72 @@
 package com.app.proyectpolleria
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class Activity_login : AppCompatActivity() {
+
+    private lateinit var btnLogin: Button
+    private lateinit var btnregister: Button
+    private lateinit var txtcorreo: EditText
+    private lateinit var txtcontra: EditText
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        btnLogin= findViewById(R.id.btn_login)
+        btnregister= findViewById(R.id.btn_register)
+        txtcorreo = findViewById(R.id.txt_correo)
+        txtcontra = findViewById(R.id.txt_contra)
+
+        singIn()
+
+        btnregister.setOnClickListener {
+            startActivity(Intent(this, Activity_register::class.java))
+        }
+
+    }
+
+    private fun singIn(){
+        title = "Sing In"
+
+        btnLogin.setOnClickListener {
+            if (!txtcorreo.text.toString().isEmpty() && !txtcontra.text.toString().isEmpty()) {
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(
+                    txtcorreo.text.toString(),
+                    txtcontra.text.toString()
+                ).addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        // Éxito al acceder
+                        showHome(task.result?.user?.email ?: "")
+                    } else {
+                        showAlert()
+                    }
+                }
+            }
+
+        }
+    }
+
+    private fun showAlert(){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ah producido un error de logeo de usuario")
+        builder.setPositiveButton("Aceptar",null)
+        val dialog: AlertDialog =builder.create()
+        dialog.show()
+    }
+
+    private fun showHome(email:String ){
+        val homeIntent= Intent(this,Home::class.java).apply {
+            putExtra("email",email)
+        }
+        startActivity(homeIntent)
     }
 }
--- app/src/main/java/com/app/proyectpolleria/Activity_register.kt
+++ app/src/main/java/com/app/proyectpolleria/Activity_register.kt
@@ -1,11 +1,57 @@
 package com.app.proyectpolleria
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
 
 class Activity_register : AppCompatActivity() {
+
+    private lateinit var btnauntentication: Button
+    private lateinit var txtnewcorreo: EditText
+    private lateinit var txtnewcontra: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        btnauntentication= findViewById(R.id.btn_auntentication)
+        txtnewcorreo = findViewById(R.id.txt_new_correo)
+        txtnewcontra = findViewById(R.id.txt_new_contra)
+
+        setup()
+
     }
+
+    private fun setup() {
+        title = "Autenticación"
+
+        btnauntentication.setOnClickListener {
+            if (!txtnewcorreo.text.toString().isEmpty() && !txtnewcontra.text.toString().isEmpty()) {
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(
+                    txtnewcorreo.text.toString(),
+                    txtnewcontra.text.toString()
+                ).addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        // Éxito al registrar
+                        startActivity(Intent(this, Activity_login::class.java))
+                    } else {
+                        showAlert()
+                    }
+                }
+            }
+
+        }
+    }
+
+    private fun showAlert(){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ah producido un error de autentificación de usuario")
+        builder.setPositiveButton("Aceptar",null)
+        val dialog: AlertDialog=builder.create()
+        dialog.show()
+    }
+
 }
