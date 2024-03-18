dd4b37e58c67147d7bedf10f3f48cd51ab69168a
Gabriel DarKreitos <inazuma123987@gmail.com>
2024-03-18 12:17:06 -0500

    .add

--- app/src/main/java/com/app/proyectpolleria/Activity_login.kt
+++ app/src/main/java/com/app/proyectpolleria/Activity_login.kt
@@ -1,12 +1,16 @@
 package com.app.proyectpolleria
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.Firebase
+import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class Activity_login : AppCompatActivity() {
 
@@ -15,6 +19,7 @@ class Activity_login : AppCompatActivity() {
     private lateinit var txtcorreo: EditText
     private lateinit var txtcontra: EditText
 
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +29,10 @@ class Activity_login : AppCompatActivity() {
         btnregister= findViewById(R.id.btn_register)
         txtcorreo = findViewById(R.id.txt_correo)
         txtcontra = findViewById(R.id.txt_contra)
+
+
+
+
 
         singIn()
 
@@ -44,7 +53,14 @@ class Activity_login : AppCompatActivity() {
                 ).addOnCompleteListener { task ->
                     if (task.isSuccessful) {
                         // Éxito al acceder
-                        showHome(task.result?.user?.email ?: "")
+                        //val email = task.result?.user?.email ?: ""
+                        val preferences = getSharedPreferences("sesion", Context.MODE_PRIVATE)
+                        val email = preferences.getString("email", null)
+                        if (email != null) {
+                            // El usuario ya ha iniciado sesión, redirigirlo a la pantalla principal
+                            showHome(email)
+                        }
+                        guardarEstadoInicioSesion(task.result?.user?.email ?: "")
                     } else {
                         showAlert()
                     }
@@ -52,6 +68,13 @@ class Activity_login : AppCompatActivity() {
             }
 
         }
+    }
+
+    private fun guardarEstadoInicioSesion(email: String) {
+        val preferences = getSharedPreferences("sesion", Context.MODE_PRIVATE)
+        val editor = preferences.edit()
+        editor.putString("email", email)
+        editor.apply()
     }
 
     private fun showAlert(){
