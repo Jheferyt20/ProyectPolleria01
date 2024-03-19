7652c784f6edd2c2cb39bdd9401153957be63609
Jenni <jenniferlisethfernandez12345@gmail.com>
2024-03-19 01:46:35 -0500

    .add

---
+++ app/src/main/java/com/app/proyectpolleria/descripcion_c.kt
@@ -0,0 +1,11 @@
+package com.app.proyectpolleria
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class descripcion_c : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_descripcion_c)
+    }
+}
---
+++ app/src/main/java/com/app/proyectpolleria/politica_empresa.kt
@@ -0,0 +1,11 @@
+package com.app.proyectpolleria
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class politica_empresa : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_politica_empresa)
+    }
+}
