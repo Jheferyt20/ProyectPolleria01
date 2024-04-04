3817545bc5e99dbf08d9a06d380b5e4bb898adbe
Paulo Ramos <anuelitoramos@gmail.com>
2024-04-04 16:49:51 -0500

    primera parte

--- app/src/main/java/com/app/proyectpolleria/descripcion_c.kt
+++ app/src/main/java/com/app/proyectpolleria/descripcion_c.kt
@@ -2,17 +2,51 @@ package com.app.proyectpolleria
 
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.ScaleAnimation
 import android.widget.ImageButton
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 
 class descripcion_c : AppCompatActivity() {
 
     private lateinit var btnBack: ImageButton
+
+    private lateinit var imgFavoritoSin : ImageView
+    private lateinit var imgFavoritoPintado : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_descripcion_c)
+        imgFavoritoPintado = findViewById(R.id.favorito_pintado)
+        imgFavoritoSin = findViewById(R.id.favorito_sin)
 
+        imgFavoritoPintado.visibility = View.GONE
+
+        imgFavoritoSin.setOnClickListener{
+            if (imgFavoritoSin.visibility == View.VISIBLE) {
+                imgFavoritoPintado.visibility = View.VISIBLE
+                imgFavoritoSin.visibility = View.GONE
+                animateLike(imgFavoritoPintado) // Pasar imgFavoritoPintado como argumento
+            } else {
+                imgFavoritoSin.visibility = View.VISIBLE
+                imgFavoritoPintado.visibility = View.GONE
+                animateLike(imgFavoritoSin) // Pasar imgFavoritoSin como argumento
+            }
+        }
+
+        imgFavoritoPintado.setOnClickListener{
+            if (imgFavoritoPintado.visibility == View.VISIBLE) {
+                imgFavoritoSin.visibility = View.VISIBLE
+                imgFavoritoPintado.visibility = View.GONE
+                animateLike(imgFavoritoSin) // Pasar imgFavoritoSin como argumento
+            } else {
+                imgFavoritoPintado.visibility = View.VISIBLE
+                imgFavoritoSin.visibility = View.GONE
+                animateLike(imgFavoritoPintado) // Pasar imgFavoritoPintado como argumento
+            }
+        }
 
 
         btnBack = findViewById(R.id.btn_back)
@@ -21,7 +55,26 @@ class descripcion_c : AppCompatActivity() {
             goBack()
         }
 
+
     }
+
+    private fun animateLike(imageView: ImageView) {
+
+        imageView.clearAnimation()
+
+        val scaleAnimation = ScaleAnimation(
+            1f, 1.2f,
+            1f, 1.2f,
+            Animation.RELATIVE_TO_SELF, 0.5f,
+            Animation.RELATIVE_TO_SELF, 0.5f
+        )
+        scaleAnimation.duration = 200
+        scaleAnimation.repeatMode = Animation.REVERSE
+        scaleAnimation.repeatCount = 1
+
+        imageView.startAnimation(scaleAnimation)
+    }
+
 
 
 
