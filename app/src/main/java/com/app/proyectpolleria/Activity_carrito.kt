d8f1b2e4acd8e522a205bac78a3abd5bf95c7932
Jheferson <jhefersoncaramelitogmail.com>
2024-04-10 13:40:09 -0500

    .add

--- app/src/main/java/com/app/proyectpolleria/Activity_carrito.kt
+++ app/src/main/java/com/app/proyectpolleria/Activity_carrito.kt
@@ -1,11 +1,17 @@
 package com.app.proyectpolleria
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 
 class Activity_carrito: AppCompatActivity() {
     private lateinit var btnBack: Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,4 +28,5 @@ class Activity_carrito: AppCompatActivity() {
         // Finaliza la actividad actual
         finish()
     }
+
 }
