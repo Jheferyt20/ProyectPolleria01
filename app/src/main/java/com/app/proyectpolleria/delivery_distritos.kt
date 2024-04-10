16097bf4c472c516bc58ce27505638efbc594659
Jenni <jenniferlisethfernandez12345@gmail.com>
2024-04-10 18:41:31 -0500

    .add

--- app/src/main/java/com/app/proyectpolleria/delivery_distritos.kt
+++ app/src/main/java/com/app/proyectpolleria/delivery_distritos.kt
@@ -2,10 +2,24 @@ package com.app.proyectpolleria
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.cardview.widget.CardView
 
 class delivery_distritos : AppCompatActivity() {
+    private lateinit var btnBack: CardView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_delivery_distritos)
+
+        btnBack = findViewById(R.id.btnAtras)
+
+        btnBack.setOnClickListener {
+            goBack()
+        }
+    }
+
+    private fun goBack() {
+        // Finaliza la actividad actual
+        finish()
     }
 }
